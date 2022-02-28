35fd4bd590a1b8726708171e50b7a7eed99d4c0b
김종혁 <kimjonghyeok@gimjonghyeog-ui-MacBookPro.local>
2022-02-28 19:44:58 +0900

    fix #1

---
+++ petBook-Back/src/test/java/com/psgstellar/petbook/PetBookBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psgstellar.petbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBookBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
